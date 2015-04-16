ee8f4c728514c9396a37347d5745920800189505
fitosegrera <fitosegrera@gmail.com>
2015-04-15 20:49:15 -0400

    redis stuff

--- nodejs/node-redis/app.js
+++ nodejs/node-redis/app.js
@@ -5,9 +5,17 @@ var client = redis.createClient()
 
 app.get('/', function(req, res) {
 	// client.set("visitors", 0)
-	client.incr("visitors", function(err, reply){
-		res.send('<p>You are visitor # ' + reply + '</p>')
-	})	
+	client.lpush("visitors", req.ip, function(err, reply){
+		res.send('<p>You are visitor # ' 
+			+ reply + ' and your IP is: ' 
+			+ req.ip + '</p>')
+	})
+})
+
+app.get('/most-recent', function(req, res){
+	client.lindex("visitors", 0, function(err, visitorCount){
+		res.send("The most recent was IP: " + visitorCount)
+	})
 })
 
 app.listen(3000)
